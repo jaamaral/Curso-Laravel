516886b2090e09f04b90eda0fd7220edfb5d135c
Jose Junqueira <jaamaral@gmail.com>
2019-10-27 17:16:16 -0300

    Salvar imagem com dropbox

--- resources/views/livro/mostrar.blade.php
+++ resources/views/livro/mostrar.blade.php
@@ -1,4 +1,7 @@
 <div>{{$livro->titulo}}</div>
 <div>{{$livro->isbn}}</div>
 <div>{{$livro->autor}}</div>
-<div><img src="{{Storage::url("imagens/fotos/$livro->foto")}}" alt="Imagem do Livro"></div>
+<div><img src="{{$livro->foto}}" alt="Imagem do Livro"></div>
+
+<!--Código utilizado quando é para mostrar imagem armazenada em disco público local
+    <div><img src="/*Storage::url("imagens/fotos/$livro->foto")*/}}" alt="Imagem do Livro"></div>-->
